64db02e82cffe87fa4c85fac430661ea984b5a4a
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2024-11-28 16:04:29 +0100

    plot th question

--- Homework2/truc.py
+++ Homework2/truc.py
@@ -21,11 +21,9 @@ W_hat = np.array([
 
 
 _, sigm_k, _ = np.linalg.svd(W_k)
-#lambd_k , _ = np.linalg.eig(W_k)
 _, sigm_hat, _ = np.linalg.svd(W_hat)
 
 print(sigm_k) #sqrt(36), sqrt(8), sqrt(8), sqrt(4) si k1 = 1, k2 = 2, k3 = 3
 print(sigm_hat) #sqrt(20), sqrt(8)
-#print(lambd_k)
 
 #print(sigm_hat[0]/sigm_k[0])
